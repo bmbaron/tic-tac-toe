31be5c5f0175b108fe2bd9d4a95c6bcbaadc649d
Ben Baron <benjamin.m.baron@gmail.com>
2021-06-27 19:42:13 +0700

    made the gameboard module and player factory function, updated CSS and HTML with inputs for players

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,87 @@
-window.onload = (event) => {
+
+const gameBoard = (() => {
+
+  const squares = ['','','','','','','','',''];
+  const getSquares =()=> {
+    return squares;
+  };
+  
+  const gridContainer = document.getElementById('grid-container');
+
+  const buildBoard = (boardSquares) => {
+    
+    boardSquares.forEach((value,index) => {
+    value = document.createElement('div');
+    value.classList.add('square');
+    value.innerHTML = index;
+    value.addEventListener("click", function() {
+      if(index%2 === 0) {
+        value.classList.toggle('squareVirus');
+      }
+      else {
+        value.classList.toggle('squareVaccine');
+      }
+    });
+  
+    gridContainer.appendChild(value);
+    });
+  };
+  
+  return {getSquares,buildBoard};
+  
+})();
+
+
+const displayBuilder = (() => {
+  
+  const gameSquares = gameBoard.getSquares();
+  gameBoard.buildBoard(gameSquares);
+  
+  return {};
+})();
+
+
+const Player = (name, number) => {
+  const getName  = () => name;
+  const die = () => {
+    // uh oh
+  };
+  const damage = x => {
+    health -= x;
+    if (health <= 0) {
+      die();
+    }
+  };
+  const attack = enemy => {
+    // if (level < enemy.getLevel()) {
+    //   damage(1);
+    //   console.log(`${enemy.getName()} has damaged ${name}`);
+    // }
+    // if (level >= enemy.getLevel()) {
+    //   enemy.damage(1);
+    //   console.log(`${name} has damaged ${enemy.getName()}`);
+    // }
+  };
+  return {attack, damage, getName}
 };
 
-function Movie (name, genre, seen) {
-  this.name = name;
-  this.genre = genre;
-  this.seen = seen;
-  this.info = function() {
-    return ("name: " + name + ", genre: " + genre + ", seen: " + seen);
-  };
-  this.toggleSeen = function() {
-    if (this.seen === true) {
-      this.seen = false;
-    }
-    else if (this.seen === false) {
-      this.seen = true;
-    }
-    populateMovies();
-  };
-}
+
+const jimmie = Player('jim', 10);
+const badGuy = Player('jeff', 5);
+jimmie.attack(badGuy);
 
 
 
-function addMovieToLibrary(movie) {
-  movieArray.push(movie);
-}
+let statusText = document.getElementById('status-text');
+statusText.innerHTML = "goodbye";
 
+const player1 = document.getElementById('player1');
+player1.addEventListener("mouseleave", function() {
+  statusText.innerHTML = "player1: " + player1.value + "<br />" + "player2: " + player2.value;
+});
 
-function buildLibrary () {
+const player2 = document.getElementById('player2');
+player2.addEventListener("mouseleave", function() {
+  statusText.innerHTML = "player1: " + player1.value + "<br />" + "player2: " + player2.value;
+});
 
-}
-  
-
-  
-  
-  const button = document.getElementById('submit');
-  button.onclick = function () {
-    
-     const movie = new Movie (name.value, genre.value, seen.checked);
-     addMovieToLibrary(movie);
-     buildLibrary();
-
-     populateMovies();
-  };
-  
-function setMovies() {
-  
-   let savedMovies=localStorage.getItem("movieArray");
-   savedMovies=JSON.parse(savedMovies);
-  for (let i = 0; i < savedMovies.length; i++)
-  {
-    if(savedMovies[i]) {
-      const name1 = savedMovies[i].name.toString();
-      const genre1 = savedMovies[i].genre.toString();
-      const seen1 = savedMovies[i].seen;
-      const movie1 = new Movie (name1, genre1, seen1);
-      addMovieToLibrary(movie1);
-    }
-  }
-  
-  buildLibrary();
-
-}
-  
-  
-function populateMovies() {
-  localStorage.setItem("movieArray", JSON.stringify(movieArray));
-}
