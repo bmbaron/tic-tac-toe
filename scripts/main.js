4f7ae9aa70c266b0cc0df1986181efdf1b21645d
Ben Baron <benjamin.m.baron@gmail.com>
2021-06-29 23:50:40 +0700

    added the functionality of player moving and the flow of the game

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 const gameBoard = (() => {
 
   const squares = ['','','','','','','','',''];
@@ -7,204 +6,137 @@ const gameBoard = (() => {
   };
   
   const gridContainer = document.getElementById('grid-container');
-
-  const buildBoard = (boardSquares) => {
+  const getGridContainer =()=> {
+    return gridContainer;
+  };
+  
+  const buildBoard = (squares) => {
     
-    boardSquares.forEach((value,index) => {
+    squares.forEach((value,index) => {
     value = document.createElement('div');
     value.classList.add('square');
     value.id = index;
-    //value.innerHTML = index;
     value.addEventListener("click", function() {
-      
-      
-      
-      // if(index%2 === 0) {
-      //   value.classList.toggle('squareVirus');
-      // }
-      // else {
-      //   value.classList.toggle('squareVaccine');
-      // }
+      gameController.playMove(index);
     });
   
     gridContainer.appendChild(value);
     });
   };
   
-  return {getSquares,buildBoard};
+  return {getGridContainer, getSquares,buildBoard};
   
 })();
 
 
-const displayBuilder = (() => {
+
+const displayController = (() => {
   
-  const gameSquares = gameBoard.getSquares();
-  gameBoard.buildBoard(gameSquares);
+  gameBoard.buildBoard(gameBoard.getSquares());
   
-  return {};
+  const updateBoard =(p1Turn, index)=> {
+    
+    const squareToChange = document.getElementById(index);
+    if(p1Turn) {
+      squareToChange.classList.add('squareVirus');
+    }
+    else {
+      squareToChange.classList.add('squareVaccine');
+    }
+    
+  };
+  
+  return {updateBoard};
+  
 })();
 
 
-const moveTracker = (() => {
+
+const gameController = (() => {
+  
+  const statusText = document.getElementById('status-text');
+  let p1;
+  let p2;
+  let p1Turn = false;
+  
   const gameMoves = [
     [0,0,0],
     [0,0,0],
     [0,0,0]
   ];
   
-  const getMoves =()=> {
-    return gameMoves;
-  };
   
-  const makeMove = (index, player) => {
-    
-    switch(index) {
-      case 0:
-        if (gameMoves[0][0] === 0){
-          gameMoves[0][0] = player;
-        }
-        break;
-      case 1:
-        if (gameMoves[0][1] === 0){
-          gameMoves[0][1] = player;
-        }
-        break;
-      case 2:
-        if (gameMoves[0][2] === 0){
-          gameMoves[0][2] = player;
-        }
-        break;
-      case 3:
-        if (gameMoves[1][0] === 0){
-          gameMoves[1][0] = player;
-        }
-        break;
-      case 4:
-        if (gameMoves[1][1] === 0){
-          gameMoves[1][1] = player;
-        }
-        break;
-      case 5:
-        if (gameMoves[1][2] === 0){
-          gameMoves[1][2] = player;
-        }
-        break;
-      case 6:
-        if (gameMoves[2][0] === 0){
-          gameMoves[2][0] = player;
-        }
-        break;
-      case 7:
-        if (gameMoves[2][1] === 0){
-          gameMoves[2][1] = player;
-        }
-        break;
-      case 8:
-        if (gameMoves[2][2] === 0){
-          gameMoves[2][2] = player;
-        }
-        break;
-    }
-    
-  };
-
-  return {makeMove, getMoves};
-
-})();
-
-for (let i=0; i<9; i++) {
-  document.getElementById(i).addEventListener("click", function() {
-    moveTracker.makeMove(i, 'x');
-    alert(moveTracker.getMoves());
+  const player1 = document.getElementById('player1');
+  const player2 = document.getElementById('player2');
+  
+  player1.addEventListener("mouseenter", function() {
+    player1.style = "background-color: lightblue";
   });
-}
-
-
-const gameController = (() => {
+  player1.addEventListener("mouseleave", function() {
+    p1 = Player(player1.value);
+    if(player2.value !== '') { p1Turn = true; startGameMessage(); }
+  });
   
-  const playRound = () => {
-    
-  if (typeof p1 === "function") {
-    console.log('hello p1');
-  //const turn = 'p1';
-  if (p1.getTurn == 'waiting')
-  {
-    alert("go P1");
-    p1.setTurn('finished');
-    alert(p1.getTurn);
-  }
-  }
-  else {
-    console.log("no p1");
-  }
-  }
+  player2.addEventListener("mouseenter", function() {
+    player2.style = "background-color: lightblue";
+  });
+  player2.addEventListener("mouseleave", function() {
+    p2 = Player(player2.value);
+    if(player1.value !== '') { p1Turn = true; startGameMessage(); }
+  });
   
-
-  return {};
-})();
-
-
-
-const Player = (name, turn) => {
-  this.name = name;
-  this.turn = turn;
-  const getName  = () => name;
-  const getTurn  = () => turn;
-  const setTurn  = (turnStatus) => turn = turnStatus;
-  
-  const die = () => {
-    // uh oh
-  };
-  const damage = x => {
-    health -= x;
-    if (health <= 0) {
-      die();
+  const startGameMessage = () => {
+    if(p1Turn) {
+      statusText.innerHTML = 'Your move, ' + p1.getName();
+    }
+    else {
+      statusText.innerHTML = 'Your move, ' + p2.getName();
     }
   };
-  const attack = enemy => {
-    // if (level < enemy.getLevel()) {
-    //   damage(1);
-    //   console.log(`${enemy.getName()} has damaged ${name}`);
-    // }
-    // if (level >= enemy.getLevel()) {
-    //   enemy.damage(1);
-    //   console.log(`${name} has damaged ${enemy.getName()}`);
-    // }
+
+  const playMove = (index) => {
+    if(typeof p1 === 'object' && typeof p2 === 'object') {
+      if(p1Turn)
+      {
+        switch(index) {
+        case 0:
+          if (gameMoves[0][0] === 0){
+            gameMoves[0][0] = 'X';
+          }
+          break;
+        }
+        displayController.updateBoard(p1Turn, index);
+        console.log(index + " was clicked by P1");
+        p1Turn = false;
+      }
+      else
+      {
+        switch(index) {
+        case 0:
+          if (gameMoves[0][0] === 0){
+            gameMoves[0][0] = 'X';
+          }
+          break;
+        }
+        displayController.updateBoard(p1Turn, index);
+        console.log(index + " was clicked by P2");
+        p1Turn = true;
+      }
+    }
+    else {
+      alert("please enter player names");
+    }
+    
   };
   
-  //gameController.playRound();
-  return {attack, damage, getName}
+
+  return {playMove};
+  
+})();
+
+const Player = (name) => {
+  this.name = name;
+  const getName  = () => name;
+  
+  return {getName}
 };
-
-
-const jimmie = Player('jim', 10);
-const badGuy = Player('jeff', 5);
-jimmie.attack(badGuy);
-
-
-
-let statusText = document.getElementById('status-text');
-statusText.innerHTML = "goodbye";
-
-const player1 = document.getElementById('player1');
-player1.addEventListener("mouseenter", function() {
-  player1.style = "background-color: lightblue";
-});
-player1.addEventListener("mouseleave", function() {
-  statusText.innerHTML = "player1: " + player1.value + "<br />" + "player2: " + player2.value;
-  
-  const p1turn = 'waiting';
-  const p1 = Player('statusText.innerHTML');
-});
-
-const player2 = document.getElementById('player2');
-player2.addEventListener("mouseenter", function() {
-  player2.style = "background-color: pink";
-});
-player2.addEventListener("mouseleave", function() {
-  statusText.innerHTML = "player1: " + player1.value + "<br />" + "player2: " + player2.value;
-  
-  const p2turn = 'waiting';
-  const p2 = Player('statusText.innerHTML');
-});
-
---
+++ scripts/mainOld.js
@@ -0,0 +1,198 @@
+
+const gameBoard = (() => {
+
+  const squares = ['','','','','','','','',''];
+  const getSquares =()=> {
+    return squares;
+  };
+  
+  const gridContainer = document.getElementById('grid-container');
+
+  const buildBoard = (getSquares()) => {
+    
+    boardSquares.forEach((value,index) => {
+    value = document.createElement('div');
+    value.classList.add('square');
+    value.id = index;
+    //value.innerHTML = index;
+    value.addEventListener("click", function() {
+      
+      
+      
+      // if(index%2 === 0) {
+      //   value.classList.toggle('squareVirus');
+      // }
+      // else {
+      //   value.classList.toggle('squareVaccine');
+      // }
+    });
+  
+    gridContainer.appendChild(value);
+    });
+  };
+  
+  return {getSquares,buildBoard};
+  
+})();
+
+
+const displayBuilder = (() => {
+  
+  const gameSquares = gameBoard.getSquares();
+  gameBoard.buildBoard(gameSquares);
+  
+  return {};
+})();
+
+
+const moveTracker = (() => {
+  const gameMoves = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+  ];
+  
+  const getMoves =()=> {
+    return gameMoves;
+  };
+  
+  const makeMove = (index, player) => {
+    
+    switch(index) {
+      case 0:
+        if (gameMoves[0][0] === 0){
+          gameMoves[0][0] = player;
+        }
+        break;
+      case 1:
+        if (gameMoves[0][1] === 0){
+          gameMoves[0][1] = player;
+        }
+        break;
+      case 2:
+        if (gameMoves[0][2] === 0){
+          gameMoves[0][2] = player;
+        }
+        break;
+      case 3:
+        if (gameMoves[1][0] === 0){
+          gameMoves[1][0] = player;
+        }
+        break;
+      case 4:
+        if (gameMoves[1][1] === 0){
+          gameMoves[1][1] = player;
+        }
+        break;
+      case 5:
+        if (gameMoves[1][2] === 0){
+          gameMoves[1][2] = player;
+        }
+        break;
+      case 6:
+        if (gameMoves[2][0] === 0){
+          gameMoves[2][0] = player;
+        }
+        break;
+      case 7:
+        if (gameMoves[2][1] === 0){
+          gameMoves[2][1] = player;
+        }
+        break;
+      case 8:
+        if (gameMoves[2][2] === 0){
+          gameMoves[2][2] = player;
+        }
+        break;
+    }
+    
+  };
+
+  return {makeMove, getMoves};
+
+})();
+
+for (let i=0; i<9; i++) {
+  document.getElementById(i).addEventListener("click", function() {
+    moveTracker.makeMove(i, 'x');
+    alert(moveTracker.getMoves());
+  });
+}
+
+
+const gameController = (() => {
+  
+  const playRound = () => {
+    
+  if (typeof p1 === "function") {
+    console.log('hello p1');
+  //const turn = 'p1';
+  if (p1.getTurn == 'waiting')
+  {
+    alert("go P1");
+    p1.setTurn('finished');
+    alert(p1.getTurn);
+  }
+  }
+  else {
+    console.log("no p1");
+  }
+  }
+  
+
+  return {};
+})();
+
+
+
+const Player = (name) => {
+  this.name = name;
+  const getName  = () => name;
+  
+  
+  const attack = enemy => {
+    // if (level < enemy.getLevel()) {
+    //   damage(1);
+    //   console.log(`${enemy.getName()} has damaged ${name}`);
+    // }
+    // if (level >= enemy.getLevel()) {
+    //   enemy.damage(1);
+    //   console.log(`${name} has damaged ${enemy.getName()}`);
+    // }
+  };
+  
+  return {getName}
+};
+
+
+const jimmie = Player('jim', 10);
+const badGuy = Player('jeff', 5);
+jimmie.attack(badGuy);
+
+
+
+let statusText = document.getElementById('status-text');
+statusText.innerHTML = "goodbye";
+
+const player1 = document.getElementById('player1');
+player1.addEventListener("mouseenter", function() {
+  player1.style = "background-color: lightblue";
+});
+player1.addEventListener("mouseleave", function() {
+  statusText.innerHTML = "player1: " + player1.value + "<br />" + "player2: " + player2.value;
+  
+  const p1turn = 'waiting';
+  const p1 = Player('statusText.innerHTML');
+});
+
+const player2 = document.getElementById('player2');
+player2.addEventListener("mouseenter", function() {
+  player2.style = "background-color: pink";
+});
+player2.addEventListener("mouseleave", function() {
+  statusText.innerHTML = "player1: " + player1.value + "<br />" + "player2: " + player2.value;
+  
+  const p2turn = 'waiting';
+  const p2 = Player('statusText.innerHTML');
+});
+
